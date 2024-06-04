cbab4b0e7a03b275f0e83b8041807601aff580fc
HEKEPOIU <decnovpk92805@gmail.com>
2024-06-04 16:40:20 +0800

    Lab12Task2

--- src/shadow.cpp
+++ src/shadow.cpp
@@ -1,6 +1,7 @@
 #include "GLTools.h"
 #include "freeglut_std.h"
 #include "math3d.h"
+#define PI 3.14159265359f
 
 // Rotation amounts
 static GLfloat xRot = 0.0f;
@@ -13,6 +14,98 @@ GLfloat diffuseLight[] = {0.7f, 0.7f, 0.7f, 1.0f};
 GLfloat specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
 GLfloat lightPos[] = {-75.0f, 150.0f, -50.0f, 0.0f};
 GLfloat specref[] = {1.0f, 1.0f, 1.0f, 1.0f};
+
+float headAngle = 0.0f;
+float LHandAngle = 0.0f;
+float RHandAngle = 0.0f;
+float LHandTAngle = 0.0f;
+float RHandTAngle = 0.0f;
+
+float LLegAngle = 0.0f;
+float RLegAngle = 0.0f;
+float LLegTAngle = 0.0f;
+float RLegTAngle = 0.0f;
+
+void glutKeyboardFunc(unsigned char key, int x, int y) {
+  switch (key) {
+  case 'z':
+    LLegAngle += 2;
+    break;
+  case 'x':
+    LLegTAngle += 2;
+    break;
+  case 'c':
+    RLegAngle += 2;
+    break;
+  case 'v':
+    RLegTAngle += 2;
+    break;
+  case 'a':
+    LHandAngle += 2;
+    break;
+  case 's':
+    LHandTAngle += 2;
+    break;
+  case 'd':
+    RHandAngle += 2;
+    break;
+  case 'f':
+    RHandTAngle += 2;
+    break;
+  case 'w':
+    headAngle += 2;
+    break;
+
+  case 'b':
+    LLegAngle -= 2;
+    break;
+  case 'n':
+    LLegTAngle -= 2;
+    break;
+  case 'm':
+    RLegAngle -= 2;
+    break;
+  case ',':
+    RLegTAngle -= 2;
+    break;
+  case 'h':
+    LHandAngle -= 2;
+    break;
+  case 'j':
+    LHandTAngle -= 2;
+    break;
+  case 'k':
+    RHandAngle -= 2;
+    break;
+  case 'l':
+    RHandTAngle -= 2;
+    break;
+  case 'i':
+    headAngle -= 2;
+    break;
+  }
+  glutPostRedisplay();
+}
+
+void DrawSphere(float x, float y, float z, float r) {
+  { // Head
+    glPushMatrix();
+
+    glTranslatef(x, y, z);
+    glutSolidSphere(r, 16, 16);
+
+    glPopMatrix();
+  }
+}
+
+void DrawBox(float x, float y, float z, float w, float h, float d) {
+  glPushMatrix();
+  glScalef(w, h, d);
+  glTranslatef(x, y, z);
+  glutSolidCube(1.0);
+  glScalef(1, 1, 1);
+  glPopMatrix();
+}
 
 // Transformation matrix to project shadow
 M3DMatrix44f shadowMat;
@@ -38,178 +131,70 @@ void DrawJet(int nShadow) {
   glVertex3f(0.0f, 0.0f, 60.0f);
   glVertex3f(-15.0f, 0.0f, 30.0f);
   glVertex3f(15.0f, 0.0f, 30.0f);
+  glEnd();
 
-  // Verticies for this panel
-  {
-    M3DVector3f vPoints[3] = {
-        {15.0f, 0.0f, 30.0f}, {0.0f, 15.0f, 30.0f}, {0.0f, 0.0f, 60.0f}};
+  // DrawSphere(0, 0, 0, 10);
+  glTranslatef(0, -20, 0);
+  // glColor3ub(255, 255, 0);
+  DrawBox(0, 0, 0, 35, 50, 10); // body
 
-    // Calculate the normal for the plane
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
+  glPushMatrix();
+  glRotatef(headAngle, 0, 0, 1); // Head rotation
+  glTranslatef(0, 40, 0);
+  DrawBox(0, 0, 0, 20, 20, 10); // Head
+  glPopMatrix();
 
-  {
-    M3DVector3f vPoints[3] = {
-        {0.0f, 0.0f, 60.0f}, {0.0f, 15.0f, 30.0f}, {-15.0f, 0.0f, 30.0f}};
+  glPushMatrix();
+  glRotatef(LHandAngle, 0, 0, 1); // LHand rotation
+  glTranslatef(-40, 10, 0);
+  DrawBox(0, 0, 0, 40, 10, 10); // LHand
 
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
+  glPushMatrix();
+  glRotatef(LHandTAngle, 0, 0, 1); // LHand_T rotation
+  glTranslatef(-50, 0, 0);
+  DrawBox(0, 0, 0, 40, 10, 10); // LHand_T
+  glPopMatrix();
 
-  // Body of the Plane ////////////////////////
-  {
-    M3DVector3f vPoints[3] = {
-        {-15.0f, 0.0f, 30.0f}, {0.0f, 15.0f, 30.0f}, {0.0f, 0.0f, -56.0f}};
+  glPopMatrix();
 
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
+  glPushMatrix();
+  glRotatef(RHandAngle, 0, 0, 1); // RHand rotation
+  glTranslatef(40, 10, 0);
+  DrawBox(0, 0, 0, 40, 10, 10); // RHand
 
-  {
-    M3DVector3f vPoints[3] = {
-        {0.0f, 0.0f, -56.0f}, {0.0f, 15.0f, 30.0f}, {15.0f, 0.0f, 30.0f}};
+  glPushMatrix();
+  glRotatef(RHandTAngle, 0, 0, 1); // RHand_T rotation
+  glTranslatef(50, 0, 0);
+  DrawBox(0, 0, 0, 40, 10, 10); // RHand_T
+  glPopMatrix();
 
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
+  glPopMatrix();
 
-  glNormal3f(0.0f, -1.0f, 0.0f);
-  glVertex3f(15.0f, 0.0f, 30.0f);
-  glVertex3f(-15.0f, 0.0f, 30.0f);
-  glVertex3f(0.0f, 0.0f, -56.0f);
+  glPushMatrix();
+  glRotatef(LLegAngle, 0, 0, 1); // LLeg rotation
+  glTranslatef(-10, -60, 0);
+  DrawBox(0, 0, 0, 10, 40, 10); // LLeg
 
-  ///////////////////////////////////////////////
-  // Left wing
-  // Large triangle for bottom of wing
-  {
-    M3DVector3f vPoints[3] = {
-        {0.0f, 2.0f, 27.0f}, {-60.0f, 2.0f, -8.0f}, {60.0f, 2.0f, -8.0f}};
+  glPushMatrix();
+  glRotatef(LLegTAngle, 0, 0, 1); // LLeg_T rotation
+  glTranslatef(0, -50, 0);
+  DrawBox(0, 0, 0, 10, 40, 10); // LLeg_T
+  glPopMatrix();
 
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
+  glPopMatrix();
 
-  {
-    M3DVector3f vPoints[3] = {
-        {60.0f, 2.0f, -8.0f}, {0.0f, 7.0f, -8.0f}, {0.0f, 2.0f, 27.0f}};
+  glPushMatrix();
+  glRotatef(RLegAngle, 0, 0, 1); // RLeg_T rotation
+  glTranslatef(10, -60, 0);
+  DrawBox(0, 0, 0, 10, 40, 10); // RLeg
 
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
+  glPushMatrix();
+  glRotatef(RLegTAngle, 0, 0, 1); // RLeg_T rotation
+  glTranslatef(0, -50, 0);
+  DrawBox(0, 0, 0, 10, 40, 10); // RLeg_T
+  glPopMatrix();
 
-  {
-    M3DVector3f vPoints[3] = {
-        {60.0f, 2.0f, -8.0f}, {-60.0f, 2.0f, -8.0f}, {0.0f, 7.0f, -8.0f}};
-
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
-
-  {
-    M3DVector3f vPoints[3] = {
-        {0.0f, 2.0f, 27.0f}, {0.0f, 7.0f, -8.0f}, {-60.0f, 2.0f, -8.0f}};
-
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
-
-  // Tail section///////////////////////////////
-  // Bottom of back fin
-  glNormal3f(0.0f, -1.0f, 0.0f);
-  glVertex3f(-30.0f, -0.50f, -57.0f);
-  glVertex3f(30.0f, -0.50f, -57.0f);
-  glVertex3f(0.0f, -0.50f, -40.0f);
-
-  {
-    M3DVector3f vPoints[3] = {
-        {0.0f, -0.5f, -40.0f}, {30.0f, -0.5f, -57.0f}, {0.0f, 4.0f, -57.0f}};
-
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
-
-  {
-    M3DVector3f vPoints[3] = {
-        {0.0f, 4.0f, -57.0f}, {-30.0f, -0.5f, -57.0f}, {0.0f, -0.5f, -40.0f}};
-
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
-
-  {
-    M3DVector3f vPoints[3] = {
-        {30.0f, -0.5f, -57.0f}, {-30.0f, -0.5f, -57.0f}, {0.0f, 4.0f, -57.0f}};
-
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
-
-  {
-    M3DVector3f vPoints[3] = {
-        {0.0f, 0.5f, -40.0f}, {3.0f, 0.5f, -57.0f}, {0.0f, 25.0f, -65.0f}};
-
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
-
-  {
-    M3DVector3f vPoints[3] = {
-        {0.0f, 25.0f, -65.0f}, {-3.0f, 0.5f, -57.0f}, {0.0f, 0.5f, -40.0f}};
-
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
-
-  {
-    M3DVector3f vPoints[3] = {
-        {3.0f, 0.5f, -57.0f}, {-3.0f, 0.5f, -57.0f}, {0.0f, 25.0f, -65.0f}};
-
-    m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
-    glNormal3fv(vNormal);
-    glVertex3fv(vPoints[0]);
-    glVertex3fv(vPoints[1]);
-    glVertex3fv(vPoints[2]);
-  }
+  glPopMatrix();
 
   glEnd();
 }
@@ -382,6 +367,7 @@ int main(int argc, char *argv[]) {
   glutCreateWindow("Shadow");
   glutReshapeFunc(ChangeSize);
   glutSpecialFunc(SpecialKeys);
+  glutKeyboardFunc(glutKeyboardFunc);
   glutDisplayFunc(RenderScene);
   SetupRC();
   glutMainLoop();
